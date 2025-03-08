456c03125da8d65b517aa5e4c6301a1b70038045
Chester J. Ayala <chesterayala.ca@gmail.com>
2025-03-08 22:07:13 +0800

    Remove login requirement for homepage

--- DepInfoCare/Pages/Index.cshtml.cs
+++ DepInfoCare/Pages/Index.cshtml.cs
@@ -1,9 +1,7 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DepInfoCare.Pages
 {
-    [Authorize]
     public class IndexModel : PageModelBase
     {
         public IActionResult OnGet()
